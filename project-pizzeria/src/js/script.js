4e3ec25f1b01e7b12738ea7c8b8e9f1ab8c5962f
Anna Grabowska <aniagoodness@gmail.com>
2020-04-08 18:28:56 +0200

    priceOrder2

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -51,7 +51,7 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
-  class Product{
+  class Product {
     constructor(id, data){
       const thisProduct = this;
       thisProduct.id = id;
@@ -63,7 +63,7 @@
       thisProduct.processOrder();
       //console.log('new Product:', thisProduct);
     }
-    renderInMenu(){
+    renderInMenu() {
       const thisProduct = this;
       /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
@@ -74,18 +74,18 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
-    getElements(){
+    getElements() {
       const thisProduct = this;
     
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
-      console.log(thisProduct.formInputs);p
+      console.log(thisProduct.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       console.log(thisProduct.priceElem);
-    
-    initAccordion(){
+    }
+    initAccordion() {
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
@@ -133,38 +133,37 @@
       const thisProduct = this;
       /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
       /* set variable price to equal thisProduct.data.price */
-      const variablePrice = thisProduct.data.price('.price');
+      let price = thisProduct.data.price;
       /* START LOOP: for each paramId in thisProduct.data.params */
-      for (let paramId in thisProduct.data.params){
+      for (let paramId in thisProduct.data.params) {
         /* save the element in thisProduct.data.params with key paramId as const param */
-        const param = thisProduct.data.params('paramId');
+        const param = thisProduct.data.params[paramId];
         /* START LOOP: for each optionId in param.options */
-        for (let optionId in param.options){
+        for (let optionId in param.options) {
           /* save the element in param.options with key optionId as const option */
-          const option = param.options('optionId');
+          const option = param.options[optionId];
           const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
           /* START IF: if option is selected and option is not default */
-          if(optionSelected && !option.default){
+          if(optionSelected && !option.default) {
             /* add price of option to variable price */
             price = price + option.price;
           /* END IF: if option is selected and option is not default */
           }
           /* START ELSE IF: if option is not selected and option is default */
-          else if(!ptionSelected && option.default){
+          else if(!optionSelected && option.default) {
             /* deduct price of option from price, odjąć cenę opcji od ceny*/
             price = price - option.price;
           /* END ELSE IF: if option is not selected and option is default */
-          }
+          } 
         /* END LOOP: for each optionId in param.options */
-        }
+        } 
       /* END LOOP: for each paramId in thisProduct.data.params */
       }
-    /* set the contents of thisProduct.priceElem to be the value of variable price */
-    }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      thisProduct.priceElem.innerHTML = price; 
+    }   
   }
-}
   const app = {
     initMenu: function(){
       const thisApp = this;
